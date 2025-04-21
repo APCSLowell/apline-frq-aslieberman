89ae4890fc308157b5a2458aea181894df381fe5
aslieberman <aslieberman@s.sfusd.edu>
2025-04-21 09:13:21 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,22 @@
-public class APLine
-{
-  /* your code here */
-  
+public class APLine {
+    private int a;
+    private int b;
+    private int c;
+
+    public APLine(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getSlope() {
+        if (b == 0) {
+            return Double.NaN; // Represents undefined slope (vertical line)
+        }
+        return -(double) a / b;
+    }
+
+    public boolean isOnLine(int x, int y) {
+        return (a * x + b * y + c == 0);
+    }
 }
